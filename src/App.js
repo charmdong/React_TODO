909d610b30712b3b19b47155336919d296fb39d5
Donggun.Chung <chungdk1117@gmail.com>
2020-11-01 16:19:12 +0900

    [수정, 삭제 기능 및 렌더링 최적화]

--- src/App.js
+++ src/App.js
@@ -3,26 +3,40 @@ import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
-const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트의 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링해 보기',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '일정 관리 앱 만들어 보기',
+function createBulkTodos() {
+  const array = [];
+  for(let index = 1; index <= 2500; index++) {
+    array.push({
+      id: index,
+      text: `할 일 ${index}`,
       checked: false,
-    },
-  ]);
+    });
+  }
+  return array;
+}
 
-  const nextId = useRef(4);
+const App = () => { 
+  const [todos, setTodos] = useState(createBulkTodos
+  //   [
+  //   {
+  //     id: 1,
+  //     text: '리액트의 기초 알아보기',
+  //     checked: true,
+  //   },
+  //   {
+  //     id: 2,
+  //     text: '컴포넌트 스타일링해 보기',
+  //     checked: true,
+  //   },
+  //   {
+  //     id: 3,
+  //     text: '일정 관리 앱 만들어 보기',
+  //     checked: false,
+  //   },
+  // ]
+  );
+
+  const nextId = useRef(2501);
 
   const onInsert = useCallback(
     text => {
@@ -32,16 +46,34 @@ const App = () => {
         cheked: false,
       };
 
-      setTodos(todos.concat(todo));
+      setTodos(todos => todos.concat(todo));
       nextId.current += 1;
     },
-    [todos]
+    []
   );
+
+  const onRemove = useCallback(
+    id => {
+      setTodos(todos => todos.filter(todo => todo.id !== id));
+    },
+    [],
+  );
+
+  const onToggle = useCallback(
+    id => {
+      setTodos(todos => 
+        todos.map(todo => 
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [],
+  )
 
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert}/>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
     </TodoTemplate>
   );
 };
